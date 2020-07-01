0bdebab694e5d08a2313a4371d24aa5378242ca6
michalmajewskii <michalmajewski.contact@gmail.com>
2020-07-02 00:09:26 +0200

    Update Start Buttons

--- lib/views/start_view.dart
+++ lib/views/start_view.dart
@@ -20,20 +20,51 @@ class StartScreen extends StatelessWidget {
               ),
 
               SizedBox(height: 60,),
-              RaisedButton(
-                child: Text('ALREADY HAVE ACCOUNT'),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SignInScreen()));
 
-                },
+              GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> SignInScreen()));
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width*0.5,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.deepOrange,
+                              Colors.deepOrangeAccent
+                            ]
+                        ),
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    child: Text("ALREADY HAVE ACCOUNT", style: TextStyle(
+                        fontSize: 15, color: Colors.white
+                    ),
+                    ),
+                  )
               ),
-              RaisedButton(
-                  child: Text('CREATE NEW ACCOUNT'),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUpScreen()));
+              SizedBox(height: 15,),
 
-                },
+              GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUpScreen()));
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width*0.5,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    child: Text('CREATE NEW ACCOUNT', style: TextStyle(
+                        fontSize: 15, color: Colors.deepPurple
+                    ),
+                    ),
+                  )
               ),
+
               SizedBox(height: 50,),
             ]),
       ),
